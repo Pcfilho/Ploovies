7dbc3a8f6be07bde54215a53df075fc7885ea0ce
Paulo <paulofilhobarroso@gmail.com>
2023-04-28 13:13:39 -0300

    feat: add Stars component

--- src/components/Stars/index.tsx
+++ src/components/Stars/index.tsx
@@ -1,22 +1,26 @@
-import { Container, Star } from './styles';
+import { FlatList } from "react-native";
+import { Container, Star } from "./styles";
 
 interface Props {
   vote_average: number;
 }
 
-
-export const Stars = ({ vote_average } : Props) => {
+export const Stars = ({ vote_average }: Props) => {
   return (
     <Container>
-      {[...Array(5).keys()].map((value) => {
-          const valueToCompare = value * 2;
-          const isSelected = vote_average > valueToCompare;
-          return <Star
-            key={value}
-            name={isSelected ? 'star' : 'star-border'}
+      <FlatList
+        keyExtractor={(item) => item.toString()}
+        data={[...Array(5).keys()]}
+        renderItem={({ item }) => {
+          const isSelected = vote_average > item * 2;
+          return (
+          <Star
+            name={isSelected ? "star" : "star-border"}
             size={16}
           />
-      })}
+        )}}
+        horizontal
+      />
     </Container>
   );
-}
+};
