ca6323b4266c7e9e9f63cbb90340dd9a582adbc9
Paulo <paulofilhobarroso@gmail.com>
2023-04-28 13:12:05 -0300

    feat: add Review component

---
+++ src/components/Review/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { 
+  Container,
+  Header,
+  AuthorTitle,
+  CreatedTitle,
+  Content,
+  ReviewContent,
+  ReviewUrl,
+} from './styles';
+import { Linking } from 'react-native';
+
+interface IReview {
+  author: string;
+  author_details: {
+    name: string;
+    username: string;
+    avatar_path: string;
+    rating: number;
+  };
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
+}
+
+interface IProps {
+  item: IReview;
+}
+
+export const Review = ({ item } : IProps) => {
+  const createdAt = new Date(item.created_at).toLocaleDateString();
+  return (
+    <Container>
+      <Header>
+        <AuthorTitle>Autor: {item.author}</AuthorTitle>
+        <CreatedTitle>{createdAt}</CreatedTitle>
+      </Header>
+      <Content>
+        <ReviewContent>{item.content}</ReviewContent>
+        <ReviewUrl onPress={() => Linking.openURL(item.url)}>{item.url}</ReviewUrl>
+      </Content>
+    </Container>
+  );
+}
