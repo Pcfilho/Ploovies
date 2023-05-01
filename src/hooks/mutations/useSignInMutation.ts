0a07b04cc56896ecaad160de436de988c55d73d6
Paulo <paulofilhobarroso@gmail.com>
2023-05-01 06:30:46 -0300

    chore: add useSingInMutation

---
+++ src/hooks/mutations/useSignInMutation.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query";
+import { updateUser } from "../../store/reducers/userReducer";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../service/firebase/firebase";
+import { useDispatch } from "react-redux";
+import { IUserSignIn } from "../forms/useSignInForm";
+
+export const useSignInMutation = () => {
+  const dispatch = useDispatch();
+  const signInUser = async ({ email, password }: IUserSignIn) => {
+    const signedUser = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return signedUser;
+  };
+
+  const mutation = useMutation({
+    mutationFn: (user: IUserSignIn) => signInUser(user),
+    onSuccess(data) {
+      dispatch(updateUser({
+        id: data.user.uid
+      }))
+    },
+  })
+
+  return mutation;
+}
