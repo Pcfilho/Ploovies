d4fe877d3af5ebb1226e9a1ca48472d0d3f07d31
Paulo <paulofilhobarroso@gmail.com>
2023-05-01 06:20:47 -0300

    feat: setup i18n, english, portuguese languages and types

---
+++ src/locale/languageInterface.ts
@@ -0,0 +1,48 @@
+export interface Language {
+  welcome: string;
+  password: string;
+  enter: string;
+  noAccount: string;
+  getRegistered: string;
+  goBackToLogin: string;
+  register: string;
+  name: string;
+  confirmPassword: string;
+
+
+  searchMovies: string;
+  action: string;
+  adventure: string;
+  animation: string;
+  comedy: string;
+  crime: string;
+  documentary: string;
+  drama: string;
+  family: string;
+  fantasy: string;
+  history: string;
+  horror: string;
+  music: string;
+  mistery: string;
+  romance: string;
+  scienceFiction: string;
+  tvMovie: string;
+  thriller: string;
+  war: string;
+  western: string;
+  loadMoreMovies: string;
+  noMovieFound: string;
+
+  companies: string;
+  actors: string;
+  avaliations: string;
+  author: string;
+
+  favorites: string;
+  emptyFavorites: string;
+
+  profile: string;
+  preferences: string;
+  darkMode: string;
+  language: string;
+}
