83946d021ec32dca3e509bbf65622f4792f60507
Paulo <paulofilhobarroso@gmail.com>
2023-05-01 06:34:58 -0300

    chore: setup new navigation system

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Login } from "../screens/Login";
-import { HomeRoutes } from "./home.routes";
+import { Login } from "../screens/auth/Login";
 import { useUserSelector } from "../store/reducers/userReducer";
+import { AppRoutes } from "./app.tab.routes";
+import { darkTheme, lightTheme } from "../styles/theme";
+import { ThemeProvider } from "styled-components/native";
+import { useDarkModeSelector } from "../store/reducers/darkModeReducer";
+import { StatusBar } from "expo-status-bar";
 
 export function Routes() {
-  const { id } = useUserSelector();
+  const id = useUserSelector();
+  const isDarkMode = useDarkModeSelector();
 
   return (
-    <NavigationContainer>
-      {id ? <HomeRoutes /> : <Login />}
-    </NavigationContainer>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} translucent />
+      <NavigationContainer>
+        {id ? <AppRoutes /> : <Login />}
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
