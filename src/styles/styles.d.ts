60d918b49c835aeada4b388847a6917a899f47f6
Paulo <paulofilhobarroso@gmail.com>
2023-05-01 06:17:48 -0300

    refactor: adjusting theme type

--- src/styles/styles.d.ts
+++ src/styles/styles.d.ts
@@ -1,8 +1,6 @@
 import 'styled-components';
-import theme from './theme';
+import { theme } from './theme';
 
 declare module 'styled-components' {
-    type ThemeType = typeof theme;
-
-    export interface DefaultTheme extends ThemeType {}
+    export interface DefaultTheme extends theme {}
 }
