50616999314d84b42ffce48a35193d936d7764ca
Paulo <paulofilhobarroso@gmail.com>
2023-05-01 06:31:53 -0300

    feat: add useUserDataQuery

---
+++ src/hooks/queries/useUserDataQuery.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query"
+import { doc, getDoc } from "firebase/firestore"
+import { firestore } from "../../service/firebase/firebase"
+import { useSelector } from "react-redux";
+import { useUserSelector } from "../../store/reducers/userReducer";
+import { useRefreshOnFocus } from "./useRefreshOnFocus";
+
+interface UserInterface {
+  name: string;
+  email: string;  
+}
+
+const fetchUserData = (userId: string) => {
+  const docRef = doc(firestore, `users`, userId)
+  return getDoc(docRef) as any
+}
+
+export const useUserDataQuery = () => {
+  const id = useUserSelector();
+  const {
+    data,
+    isLoading,
+    refetch
+  } = useQuery({
+    queryKey: ['@Ploovies/userData'],
+    queryFn: () => fetchUserData(id),
+  })
+
+  useRefreshOnFocus(refetch)
+
+  return {
+    data: data?.data() as UserInterface || undefined,
+    isLoading
+  }
+}
